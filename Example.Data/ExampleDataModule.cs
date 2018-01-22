b0d21653bb57ca55808b6ef23a21b83350adcec6
Emre Karahan <emre.karahan@gmail.com>
2018-01-22 23:44:22 +0300

    First Commit

---
+++ Example.Data/ExampleDataModule.cs
@@ -0,0 +1,15 @@
+﻿using Example.Data.Repository;
+using Example.Data.Repository.Impl;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Data
+{
+    public static class ExampleDataModule
+    {
+        public static IServiceCollection AddDataModule(this IServiceCollection services)
+        {
+            services.AddScoped<IDataRepository, DataRepository>();                                 
+            return services;
+        }
+    }
+}
