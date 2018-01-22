e93158c8a086aabfe21f582f1e0b342cff68d546
Emre Karahan <emre.karahan@gmail.com>
2018-01-22 22:34:02 +0300

    First Commit

---
+++ Example.Core/ExampleCoreModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Core
+{
+    public static class ExampleCoreModule
+    {
+        public static IServiceCollection AddCoreModule(this IServiceCollection services)
+        {
+            services.AddTransient<ISafeExecutor, SafeExecutor>();                                 
+            return services;
+        }
+    }
+}
